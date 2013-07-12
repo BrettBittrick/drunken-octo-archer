332cb9cc916c90f822ad48e84cf5ae367a4d085f
Brett Bittrick <brett.bittrick@gmail.com>
2013-07-12 02:01:21 -0700

    Tiles
    Updated the grass graphic, and did a basic tile render loop

--- src/main.py
+++ src/main.py
@@ -4,16 +4,36 @@ from pygame.locals import *
 pygame.init()
 fpsClock = pygame.time.Clock()
 
+#sets the window size
+windowX, windowY = 640 , 480
+
+#sets the map size (in tiles)
+mapX, mapY = 256, 256
+
+#size of the tiles
+tileSize = 16
+
+#the number of tiles that can be rendered in the window
+tilesX = (windowX / tileSize) + 1 # add .5 so we'll render a tile even if it's only partially on the map
+tilesY = (windowY / tileSize) + 1
+
 mouseX, mouseY = 0 , 0
 
-windowSurfaceObj = pygame.display.set_mode((640,480))
+windowSurfaceObj = pygame.display.set_mode((windowX,windowY))
 pygame.display.set_caption('Yay! pygame works!')
 
 tileSurfaceObj = pygame.image.load('../dat/img/grass.png')
 
 while True:
-	windowSurfaceObj.fill(pygame.Color(0,0,0))
-	windowSurfaceObj.blit(tileSurfaceObj, (mouseX,mouseY))
+	#windowSurfaceObj.fill(pygame.Color(0,0,0))
+	#windowSurfaceObj.blit(tileSurfaceObj, (mouseX,mouseY))
+
+	y = int(tilesY)
+	
+	for tileY in range(tilesY):
+		for tileX in range(tilesX):
+			windowSurfaceObj.blit(tileSurfaceObj, (tileX * tileSize, tileY * tileSize))
+			
 
 	for event in pygame.event.get():
 		if event.type == QUIT:
